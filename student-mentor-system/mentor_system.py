e9f97509f498dc9d245e11ff44cac34f8a4eab71
Krolikizlesa <Krolikizlesa75@example.com>
2025-08-18 23:26:07 +0300

    Implement assignment #1: Inheritance

---
+++ student-mentor-system/mentor_system.py
@@ -0,0 +1,20 @@
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    pass
+
+# Проверка
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)     # []
+print(reviewer.courses_attached)     # []

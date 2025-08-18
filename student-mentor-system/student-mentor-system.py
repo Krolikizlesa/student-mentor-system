042a05489615189f1693e5967a2c828e08395b7f
Krolikizlesa <Krolikizlesa75@example.com>
2025-08-18 20:25:44 +0300

    Первый коммит

---
+++ student-mentor-system/student-mentor-system.py
@@ -0,0 +1,41 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+# Тестовые данные
+best_student = Student('Ruoy', 'Eman', 'male')
+best_student.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Python', 8)
+
+print(best_student.grades)
+
